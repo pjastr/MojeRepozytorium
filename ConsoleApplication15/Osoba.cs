c128f1f2bec893bd372030f8d39a3d104fad1b8b
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-16 09:45:49 +0200

    Add files via upload

---
+++ ConsoleApplication15/Osoba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Osoba
+    {
+        internal string imie;
+        internal string nazwisko;
+        internal int rokUrodzenia;
+        public int waga;
+        public int wzrost;
+        public bool okulary;
+        public string plec;
+        public int ObliczWiek()
+        {
+            return DateTime.Now.Year - rokUrodzenia;
+        }
+
+        public string Info()
+        {
+            if (plec == "K")
+            {
+                return "Dyrektorem jest Pani " + imie + " " + nazwisko;
+            }
+            else
+            {
+                return "Dyrektorem jest Pan " + imie + " " + nazwisko;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba dyrektor = new Osoba();
+            dyrektor.imie = "Adam";
+            dyrektor.nazwisko = "kowalski";
+            dyrektor.rokUrodzenia = 1970;
+            dyrektor.waga = 90;
+            dyrektor.wzrost = 175;
+            dyrektor.okulary = true;
+            dyrektor.plec = "M";
+
+            Console.WriteLine(dyrektor.imie + "\n"+ dyrektor.nazwisko 
+                + "\n");
+            Console.WriteLine(dyrektor.ObliczWiek());
+
+            Console.WriteLine(dyrektor.Info());
+            Console.ReadKey();
+
+        }
+    }
+}
